491c6d22210249cbfa5552d93eb5f4e12dffb87e
omsaini-1441 <omsaini1441@gmail.com>
2024-04-06 12:52:00 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return <footer>
+    <p>
+        Copyright © {(new Date()).getFullYear()}. Om Saini All rights reserved.
+    </p>
+         </footer>
+
+}
+
+export default Footer;
